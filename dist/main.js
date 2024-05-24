fa835492fe7df75051c8d181d409cdfc3f6f2476
Furkan Umut Ç. <umutcomalak@gmail.com>
2024-05-24 23:59:05 +0300

    commit
    commit

---
+++ dist/main.js
@@ -0,0 +1,5 @@
+// bundled src file
+(() => {
+  "use strict";
+  console.log({ id: 1, text: "Hello World" });
+})();
---
+++ src/index.js
@@ -0,0 +1,8 @@
+// npm install -D webpack webpack-cli
+// uppercase D for dev depencency, webpack-cli for comment line interface
+
+import message from "./message.js";
+
+console.log(message);
+
+// npm run build
